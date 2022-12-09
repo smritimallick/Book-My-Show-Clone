b3e661f62b240661534349b31313ce483ca58084
smritimallick <smritimallick0204@gmail.com>
2022-12-09 14:31:54 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// React-slick css 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+function App() {
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    )
+  }
+
+export default App;
