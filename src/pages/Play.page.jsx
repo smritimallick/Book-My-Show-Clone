4c2c41f1e04348bfd2236a01d3554e10f4172109
Smriti Mallick <76815843+smritimallick@users.noreply.github.com>
2022-12-11 18:59:05 +0530

    Updated PlayPage

--- src/pages/Play.page.jsx
+++ src/pages/Play.page.jsx
@@ -11,7 +11,7 @@ const PlayPage = () => {
       <div className="container mx-auto px-4 my-10">
         <div className="w-full  flex flex-col-reverse lg:flex-row-reverse gap-4">
           <div className="lg:w-3/4 p-4 bg-white rounded">
-            <h2 className="text-2xl font-bold mb-4">Plays in NCR Delhi</h2>
+            <h2 className="text-2xl font-bold mb-4">Plays in Hyderabad</h2>
             <div className="flex flex-wrap">
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
@@ -25,15 +25,15 @@ const PlayPage = () => {
                 <Poster
                   isPlay={true}
                   src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-suhani-shah-kahaani-india-tour-0-2022-8-29-t-12-36-14.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
-                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
+                  title="Suhani Shah Kahani - INDIA Tour"
+                  subtitle="Comedy Shows | Hindi  | 13yrs + | 1hr"
                 />
               </div>
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
                   isPlay={true}
                   src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-daddy-kool-by-atul-khatri-0-2022-8-16-t-10-37-17.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
+                  title="Daddy Kool by Atul Khatri"
                   subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
                 />
               </div>
@@ -41,24 +41,24 @@ const PlayPage = () => {
                 <Poster
                   isPlay={true}
                   src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-sunburn-arena-ft-dj-snake-delhi-ncr-0-2022-8-19-t-11-31-17.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
-                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
+                  title="DJ Snake Sunburn Arena"
+                  subtitle="EDM Night | English | 15yrs + | 4hrs "
                 />
               </div>
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
                   isPlay={true}
-                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-so-rude-of-me-by-swati-sachdeva-0-2022-9-4-t-6-10-37.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
-                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
+                  src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxMSBEZWM%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00345870-khrmuaqcaq-portrait.jpg"
+                  title="Tuglaq - An Online Play"
+                  subtitle="Historical Play | Hindi | 6yrs + | 1hr 30mins"
                 />
               </div>
               <div className="w-1/2 md:w-1/3 lg:w-3/12 my-3 flex items-center">
                 <Poster
                   isPlay={true}
-                  src="https://assets-in.bmscdn.com/nmcms/events/banner/desktop/media-desktop-so-rude-of-me-by-swati-sachdeva-0-2022-9-4-t-6-10-37.jpg"
-                  title="So Rude of Me by Swati Sachdeva"
-                  subtitle="Comedy Shows | Hindi, English | 16yrs + | 1hr 30mins"
+                  src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U3VuLCAxMSBEZWM%3D,ots-29,otc-FFFFFF,oy-612,ox-24:q-80/et00344360-rybkmwtmlf-portrait.jpg"
+                  title="12 Inches"
+                  subtitle="Comedy Shows | Hindi, English | 18yrs + | 1hr 30mins"
                 />
               </div>
             </div>
